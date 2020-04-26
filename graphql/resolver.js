f6b39d57654f20a6ea4157a730d5b2e231072461
kraevskiy <ilya.labelplatform@gmail.com>
2020-04-26 14:53:13 +0300

    add && init GraphQl

---
+++ graphql/resolver.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  rest() {
+    return 'Hello graphQl'
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const path = require('path')
+const graphqlHTTP = require('express-graphql')
 const sequelize = require('./utils/database')
-const todoRoutes = require('./routes/todo')
+const schema = require('./graphql/scheme')
+const resolver = require('./graphql/resolver')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
-app.use('/api/todo', todoRoutes)
+
+app.use(graphqlHTTP({
+  schema: schema,
+  rootValue: resolver
+}))
 
 app.use((req, res, next) => {
   res.sendFile('/index.html')
